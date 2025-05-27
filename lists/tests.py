4a0982b60a901ee10bcb5696491cd0453a893864
Jitsu <jitsu233@gmail.com>
2025-05-27 16:10:14 +0800

    diff url pattern

--- lists/tests.py
+++ lists/tests.py
@@ -27,13 +27,6 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
-    def test_display_all_list_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
-        response = self.client.get("/")
-        self.assertIn("item 1", response.content.decode())
-        self.assertIn("item 2", response.content.decode())
-
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
@@ -46,19 +39,31 @@ class HomePageTest(TestCase):
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.endswith("</html>"))
 
+
+class NewListTest(TestCase):
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
     def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/")
+        self.assertRedirects(response, "/lists/the-new-page/")
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-new-page/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_all_items(self):
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+        response = self.client.get("/lists/the-new-page/")
+        self.assertContains(response, "item 1")
+        self.assertContains(response, "item 2")
