a5efc2ea4beaf264ebaa5dc0c5a8c503fb7bcf12
Jitsu <jitsu233@gmail.com>
2025-05-27 14:34:27 +0800

    reforacter

--- lists/tests.py
+++ lists/tests.py
@@ -7,13 +7,18 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode()
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        html = response.content.decode().strip()
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, "home.html")
